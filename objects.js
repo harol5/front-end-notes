6bcff04f927a53f718b1c77125dc7da584cdf65b
harol5 <hrcode95@gmail.com>
2023-08-03 22:52:40 -0400

    some code added to object.js

--- objects.js
+++ objects.js
@@ -37,6 +37,19 @@ let obj2 = {
   },
 };
 
+let obj = {
+  field: 1,
+  field2: 2,
+  field3: 3,
+  field4: 4,
+};
+
+console.log(Object.entries(obj));
+
+for (let prop of Object.entries(obj)) {
+  console.log(prop);
+}
+
 //----------------------------------------Objects----------------------------------------//
 
 function Person(name, lastName, age, sex) {
