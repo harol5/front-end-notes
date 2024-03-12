f6d0a677b066e71008f9b50e9bbe109669042741
harol5 <hrcode95@gmail.com>
2024-03-12 10:29:57 -0400

    practicing git

--- events.js
+++ events.js
@@ -1,12 +1,41 @@
 const body = document.querySelector("body");
 const main = document.querySelector("main");
 const section = document.querySelectorAll("section");
+const footer = document.querySelector("footer");
 
 const myEvent = new Event("customEvent", { bubbles: true });
 
-body.addEventListener("click", (e) => {
+document.addEventListener("customEvent", (e) => {
+  console.log("custom event handler trigger");
   console.log(e);
+});
+
+body.addEventListener("click", (e) => {
+  console.log("click event handler trigger");
   e.target.dispatchEvent(myEvent);
 });
 
-main.addEventListener("customEvent", (e) => console.log(e));
+footer.addEventListener("click", (e) => {
+  console.log("footer event handler trigger");
+  console.log(e);
+});
+
+const myMap = new Map();
+myMap.set({ name: "harol" }, "human");
+myMap.set({ name: "lilo" }, "human");
+myMap.set(78, "human");
+
+function convertHexaToDec(hex) {
+  if (typeof hex === "number") {
+    const asStringHex = hex.toString();
+    const asArray = asStringHex.split("");
+
+    console.log(asStringHex);
+    console.log(asArray);
+  }
+}
+
+const form = document.getElementsByTagName("form")[0];
+form.addEventListener("submit", (res) => {
+  console.log(res);
+});
--- modules.js
+++ modules.js
@@ -3,3 +3,5 @@ import Car from "./modules/Car";
 let myCar = new Car("bmw", 2024);
 
 myCar.startCar();
+
+console.log("im running");
